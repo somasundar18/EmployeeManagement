dd79932785ac8d36c5a01756029f68514d13f048
Somasundar G <somasundar548@gmail.com>
2026-06-29 19:31:19 +0530

    Update DBUtil.java

--- EmployeeManagement/src/in/kce/util/DBUtil.java
+++ EmployeeManagement/src/in/kce/util/DBUtil.java
@@ -11,7 +11,7 @@ public class DBUtil {
 			Class.forName("com.mysql.cj.jdbc.Driver");
 			String url = "jdbc:mysql://localhost:3306/java";;
 			String username = "root";
-			String password = "Sundar@2007";
+			String password = "Pass";
 			Connection connection = DriverManager.getConnection(url, username, password);
 			return connection;
 		} catch (ClassNotFoundException | SQLException e) {
